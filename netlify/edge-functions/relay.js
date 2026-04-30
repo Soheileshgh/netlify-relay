ab0157181a5cfe0d429d301e1bb0e89acfae9cb4
Soheileshgh <Soheil.sarabi21@gmail.com>
2026-04-30 03:53:09 +0330

    relay.js

--- netlify/edge-functions/relay.js
+++ netlify/edge-functions/relay.js
@@ -1,4 +1,4 @@
-const TARGET_BASE = (Netlify.env.get("TARGET_DOMAIN") || "").replace(/\/$/, "");
+const TARGET_BASE = (Netlify.env.get("TARGET_DOMAIN=https://s1.viasky.online:24779") || "").replace(/\/$/, "");
 
 const STRIP_HEADERS = new Set([
   "host",
